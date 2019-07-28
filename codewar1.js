3ec38dcfd847f56d79b525a41cb05666a26224f0
Serena Dominguez <serenadominguez@gmail.com>
2019-07-28 16:13:39 -0400

    message

--- codewar1.js
+++ codewar1.js
@@ -1,70 +1,19 @@
-// function maxOfTwoNumbers(num1, num2){
-//   if (num1 > num2){
-//     return num1;
-//   } else {
-//     return num2;
-//   }
-// } 
-// var words = [
-//   'mystery',
-//   'brother',
-//   'aviator',
-//   'crocodile',
-//   'pearl',
-//   'orchard',
-//   'crackpot'
-// ];
-
-// function findLongestWord(arrayOfWords){
-// let longestWord = "";
-// arrayOfWords.forEach(curentWord => {
-//   if(curentWord.length > longestWord.length){
-//     longestWord = curentWord;
-//   }
-// })
-// return longestWord;
+// function someCallbackFunction() {
+//   console.log("Hey there, Ironhackers!");
 // }
-// console.log("result: ", findLongestWord(words));
+// const timeoutId = setTimeout(someCallbackFunction, 5000);
 
-// CALCULATING SUM
+let counter = 1;
+const callbackFunction = function () {
+  console.log(counter);
+  setTimeout(callbackFunction, 1000);
 
-var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-function sumArray(arrNum) {
-  return arrNum.reduce((accumulator, currentValue) => {
-    // console.log(accumulator,"  " currentValue);
-    return accumulator + currentValue;
-  });
-}
-// console.log(sumArray(numbers));
-
-
-// CALCULATING AVERAGE 
-function averageNumbers(arrNums){
-  return sumArray(arrNums)/arrNums.length;  
+  counter += 1;
 }
 
-// console.log(averageNumbers(numbers));
+let timeoutId = setTimeout(callbackFunction, 1000);
 
-var words = [
-    'seat',
-    'correspond',
-    'linen',
-    'motif',
-    'hole',
-    'smell',
-    'smart',
-    'chaos',
-    'fuel',
-    'palace'
-];
-
-function averageWordLength(arrOfWords){
-// iterate through array 
-  let wordSum = 0;
-  arrOfWords.forEach(eachWord =>{
-    wordSum += eachWord.length;
-  });
-  return wordSum / arrOfWords.length;
+starter(){
+  setInterval this.currentTime +1
 }
-
-console.log(averageWordLength(words)); 
+let setInterval = this.intervalId 
