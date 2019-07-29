29718018c09f0fd2f0ba4cb1266844677f6b60fa
Serena Dominguez <serenadominguez@gmail.com>
2019-07-29 16:15:34 -0400

    message

--- codewar1.js
+++ codewar1.js
@@ -1,19 +1,29 @@
-// function someCallbackFunction() {
-//   console.log("Hey there, Ironhackers!");
-// }
-// const timeoutId = setTimeout(someCallbackFunction, 5000);
+// FIND THE MISSING LETTER
+//#Find the missing letter
+//Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+//You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+//The array will always contain letters in only one case.
+//Example:
+//['a','b','c','d','f'] -> 'e'
+//['O','Q','R','S'] -> 'P'
+//(Use the English alphabet with 26 letters!)
 
-let counter = 1;
-const callbackFunction = function () {
-  console.log(counter);
-  setTimeout(callbackFunction, 1000);
+// first make function findmissing array
+//make variable string--full alphabet 
+//make second variable that takes the string and makess it an array with ("") in between 
+//then make a loop that involves the length of that variable
 
-  counter += 1;
-}
 
-let timeoutId = setTimeout(callbackFunction, 1000);
-
-starter(){
-  setInterval this.currentTime +1
-}
-let setInterval = this.intervalId 
+function findMissingLetter(arr) {
+    var str = "abcdefghijklmnopqrstuvwxyz";
+    var str2arr = arr.join("");
+    for (var i = 0; i < str2arr.length; i++) {
+      if (str2arr.charCodeAt(i + 1) - str2arr.charCodeAt(i) != 1) {
+        return String.fromCharCode(str2arr.charCodeAt(i) + 1);
+      }
+    }  
+  }  
+  findMissingLetter(["a","b","c","d","f"]);
+  findMissingLetter(['O','Q','R','S']);
+  
+  
