48b812923330a69d992825bf34b306e87dae76cc
Serena Dominguez <serenadominguez@gmail.com>
2019-08-12 16:01:53 -0400

    message

--- codewar1.js
+++ codewar1.js
@@ -1,122 +1,52 @@
-// Characters Collection
-// Characters
+// 
 
-var charactersArray = [
+// function sumArray(arrayOfNumbers){
+// let total = 0;
+// arrayOfNumbers.forEach((eachSingleNumber)=>{
+// total += eachSingleNumber;
+// })
+// return total;
+// }
 
-    mrGreen = {
-    first_name:   "Jacob",
-    last_name:    "Green",
-    color:        "green",
-    description:  "He has a lot of connections",
-    age:          45,
-    image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-    occupation:   "Entrepreneur"
-    },
-    
-    drOrchid = { 
-    first_name:   "Doctor",
-    last_name:    "Orchid",
-    color:        "white",
-    description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-    age:          26,
-    image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    ocupation:   "Scientist"
-    },
-    
-    profPlum = { 
-    first_name:   "Victor",
-    last_name:    "Plum",
-    color:        "purple",
-    description:  "Billionare video game designer",
-    age:          22,
-    image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
-    occupation:   "Designer"
-    },
-    
-    missScarlet = { 
-    first_name:   "Kasandra",
-    last_name:    "Scarlet",
-    color:        "red",
-    description:  "She is an A-list movie star with a dark past",
-    age:          31,
-    image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
-    occupation:   "Actor"
-    },
-    
-    mrsPeacock = { 
-    first_name:   "Eleanor",
-    last_name:    "Peacock",
-    color:        "blue",
-    description:  "She is from a wealthy family and uses her status and money to earn popularity",
-    age:          36,
-    image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-    occupation:   "Socialité",
-    }, 
-    
-    mrMustard = { 
-    first_name:   "Jack",
-    last_name:    "Mustard",
-    color:        "yellow",
-    description:  "He is a former football player who tries to get by on his former glory",
-    age:          62,
-    image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
-    occupation:   "Retired Football player"
-    } 
-    ]
-    
-    
-    // Weapons Collection
-    // Weapons
-    var weaponsArray = [
-    
-    {name: "rope"        ,  weight:10},
-    {name: "knife"       ,  weight: 8},
-    {name: "candlestick" ,  weight: 2},
-    {name: "dumbbell"    ,  weight: 3},
-    {name: "poison"      ,  weight: 2},
-    {name: "axe"         ,  weight: 1},
-    {name: "bat"         ,  weight: 1},
-    {name: "trophy"      ,  weight: 2},
-    {name: "pistol"      ,  weight: 2}
-    
-    ];
-    
-    // Rooms' Collection
-    // Rooms
-    
-    var roomsArray = [
-    
-    {name: "Dinning Room"},
-    {name: "Conservatory"},
-    {name: "Kitchen"},
-    {name: "Study"},
-    {name: "Library"},
-    {name: "Billiard Room"},
-    {name: "Lounge"},
-    {name: "Ballroom"},
-    {name: "Hall"},
-    {name: "Spa"},
-    {name: "Living Room"},
-    {name: "Observatory"},
-    {name: "Theater"},
-    {name: "Guest House"},
-    {name: "Patio"}
-    
-    ];
-    
-    console.log(roomsArray.length);
-    console.log(charactersArray.length);
-    console.log(weaponsArray.length);
-    
-    
-    // funtion shuffle(characters, weapons, rooms){
-    //   let currentGame = 
-    // [randomItem(characters),randomItem(room), randomItem(weapons)]; 
-    // }
-    
-    // function randomItem(array){
-    //   return array[Math.floor(Math.random() * array.length)]
-    // }
-    
-      // characters[Math.floor(Math.random() * characters.length)]
-    
+// function averageNumbers(arrOfNums){
+//   if(arrOfNums.length === 0)(return undefined)
+//   return sumArray(arrOfNum)/arrOfNums.length;
+// }
+
+// function uniquifyArray(arrayOfWords){
+//   let newArray = [];
+//   arrayOfWords.forEach((eachWord)=>{
+//     eachWord
+//   })
+// }
+
+// FINDING ELEMENTS
+// let result = false; 
+// ^default
+
+// function greatestProduct(matrix){
+  
+//   let greatest = 0;
+//   matrix.forEach((eachRow)=>{
+//       for(let i = 0, i < eachRow.length -4, i++){
+//         let product = eachRow[i]*eachRow[i+1]*eachRow[i+2]*eachRow[i+3];
+//         if(product . greatest){
+//           console.log(eachRow[i], eachRow[i+1], eachRow[i+2], eproduct)
+//           greatest = product; 
+//         }
+//     }
+//   })
+
+//     for(let i=0; i< matrix.length -4; i++){
+      
+//       matrix[i].forEach((eachNumber, index))=>{
+
+
+//       matrix[i][0]
+//       matrix[i+1][0]
+//       }
+//     }
+
+// return greatest;
+
+// }
