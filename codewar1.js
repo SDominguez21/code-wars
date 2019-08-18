5a6afdb354269f1e20d69cfeea39c40f4cb0ef97
Serena Dominguez <serena@Serenas-MacBook-Pro.local>
2019-08-18 15:52:00 -0400

    message

--- codewar1.js
+++ codewar1.js
@@ -1,102 +1,14 @@
-// var x = 10;
+var emptyArray = []
 
-// x++;
+function doublePush(blah) {
+  emptyArray.push(blah);
+  emptyArray.push(blah);
+}
 
-// x+=1;
+doublePush("the eagle");
 
-// console.log(x);
+console.log(emptyArray);
 
-// console.log(5 >= 5);
+doublePush("the eagle");
 
-// console.log(5 != "5");
-
-// var theTalonColor = "black";
-
-//var myBirdObject = {
-  // wings: true,
-  // type: "eagle", 
-  // beakLenth: 3,
-  // talon: theTalon = {
-  //  talonLength: 5,
-  //  color: theTalonColor,
-  // }
-// }
-
-// console.log(myBirdObject.type);
-// console.log(myBirdObject.talon);
-// console.log(myBirdObject.talon.talonLength);
-// console.log(myBirdObject.wings);
-
-// myBirdObject.wings = "I have wings, Hurray!!!";
-
-// const newObj = {}
-
-// console.log(newObj);
-
-// newObj.name = "the bird with wings";
-
-// console.log(newObj);
-
-var theTalonColor = "black"
-
-var myBirdObject = {
-    wings: true,
-    type: "eagle", 
-    beakLenth: 3,
-    talon: theTalon = {
-    talonLength: 5,
-    color: theTalonColor,
-   }
- }
-
-var myNameArray = []
-
-console.log(myNameArray);
-
-myNameArray.push("Max");
-
-console.log(myNameArray);
-
-myNameArray.push("Candito", "Elena");
-
-console.log(myNameArray);
-
-myNameArray.unshift("Roy", "Johan");
-
-console.log(myNameArray);
-
-var secondArrayOfNames = ["Jack", "Marcos"];
-
-myNameArray.push(secondArrayOfNames);
-
-console.log(myNameArray);
-
-console.log(myNameArray[1]);
-
-console.log(myNameArray[5][0]);
-
-myNameArray[5].push("Loic");
-
-console.log(myNameArray);
-
-myNameArray.splice( 1, 0, "Serena");
-
-console.log(myNameArray);
-
-myNameArray.splice(3, 0, "Marcus");
-
-console.log(myNameArray);
-
-myNameArray.splice(4);
-
-console.log(myNameArray);
-
-myNameArray.splice(4)
-
-console.log(myNameArray)
-
-myNameArray.splice(4, 8, myBirdObject)
-
-console.log(myNameArray)
-
-
+console.log(emptyArray)
